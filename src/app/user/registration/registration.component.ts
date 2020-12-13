a480b5eef2eb8654b9031010e1711e7a3c6c988d
abodziacka <olabodziacka@op.pl>
2020-12-13 18:14:42 +0100

    reg1

--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Toast, ToastrService } from 'ngx-toastr';
+import { UserService } from 'src/app/shared/user.service';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service: UserService, public toastr:ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    (this.service.register()).subscribe(
+      (res: any) => {
+        console.log(res);
+        if (res.succeeded) {
+          this.service.formModel.reset();
+          this.toastr.success('New user created!', 'Registration succesful');
+
+          
+        }
+        else {
+          res.errors.forEach((element: { code: any; description: string | undefined; }) => {
+            switch (element.code) {
+              case 'DuplicateUserName':
+                this.toastr.error('Username is already taken.', 'Registration failed.');
+
+                //Username id already taken
+                break;
+              default:
+                this.toastr.error(element.description, 'Registration failed.');
+
+                //Registration failed
+                break;
+
+            }
+          });
+        }
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
 }
